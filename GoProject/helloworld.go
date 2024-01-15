4ad5545d3fa070108fc1f781cdfcf1fc66c2acf0
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-01-15 20:54:44 +0700

    ประกาศตัวแปร name ด้วย var ไว้ภายในฟังก์ชั่น main และประกาศ  ตัวแปร age ด้วย var ไว้ภายนอกฟังก์ชัน main

--- GoProject/helloworld.go
+++ GoProject/helloworld.go
@@ -2,23 +2,17 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
 /*
-แสดงการทำงานของ type infrernce
-
-var จะประกาศตัวแปรไว้ภายใน หรือ ภายนอก ฟังก์ชั่นก็ได้
-:= จะต้องประกาศตัวแปรไว้ภายในฟังก์ชั่นใด ฟังก์ชั่นหนึ่งเท่านั้น
-
+ ประกาศตัวแปร name ด้วย var ไว้ภายในฟังก์ชั่น main และประกาศ
+ ตัวแปร age ด้วย var ไว้ภายนอกฟังก์ชัน main
 */
+
+var age int = 18
+
 func main() {
-	str := "my number"
-	num := 28
-	status := true
-
-	fmt.Println("type inference of str variable is ", reflect.TypeOf(str))
-	fmt.Println("type inference of num variable is ", reflect.TypeOf(num))
-	fmt.Println("type inference of status variable is ", reflect.TypeOf(status))
-
+	var name string = "Orapin"
+	fmt.Println("Hello World", name)
+	fmt.Println("Your age is ", age)
 }
