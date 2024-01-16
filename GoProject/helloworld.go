fa1c2f134ca581f6bf33d367a9ae819d6a1a9b70
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-01-16 21:01:46 +0700

    การประกาศตัวแปรหลายตัว

--- GoProject/helloworld.go
+++ GoProject/helloworld.go
@@ -1,15 +1,17 @@
 package main
 
-import (
-	"fmt"
-)
-
 /*
- ประกาศตัวแปร name ด้วย := ไว้นอก functions ผิด
+ การประกาศตัวแปรหลายตัว
 */
 
-name := "Orapin"
-
 func main() {
-	fmt.Println("Hello World! ", name)
+	var num1, num2, num3 = 10, 20, 30
+	var n1, n2, n3 int = 100, 200, 300
+	var (
+		product = "Perfume"
+		price   = 2500
+		inStock = true
+	)
+	str1, str2 := "aaa", "bbb"
+	name, age := "Somsri", 18
 }
