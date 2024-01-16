c71200cfb73a90024e68bca0fd6ebbaedd911890
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-01-16 21:09:46 +0700

    แสดงการประกาศตัวแปรหลายตัว V2

--- GoProject/helloworld.go
+++ GoProject/helloworld.go
@@ -7,11 +7,21 @@ import (
 /*
 แสดงการประกาศตัวแปรหลายตัว
 */
-var prd_id1, prd_id2 = "D001", "D002"
 
 func main() {
-	prd_name1, price1 := "T-Shirt", 250
-	prd_name2, price2 := "Shoe", 499
+
+	var (
+		prd_id1   = "D001"
+		prd_name1 = "T-Shirt"
+		price1    = 250
+	)
+
+	var (
+		prd_id2   = "D002"
+		prd_name2 = "Shoe"
+		price2    = 499
+	)
+
 	fmt.Println("Product : ", prd_id1, " | ", prd_name1, " | ", price1, "Baht")
 	fmt.Println("product : ", prd_id2, " | ", prd_name2, " | ", price2, "Baht")
 }
