b48e60ec2e3084171b27f852956de09417ede8e4
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-01-21 23:22:28 +0700

    แสดงการทำงานของตัวดำเนินการทางคณิตศาสตร์

--- GoProject/helloworld.go
+++ GoProject/helloworld.go
@@ -5,26 +5,19 @@ import (
 )
 
 /*
-ในภาษา Go, ข้อมูลชนิด string ถือว่าเป็น "immutable" หรือ ไม่สามารถเปลี่ยนแปลงได้
+แสดงการทำงานของตัวดำเนินการทางคณิตศาสตร์
 
 */
 
 func main() {
-	// ประกาศตัวแปรเป็นชนิดข้อมูลสตริง
-	str := "Hello, world!"
-	// ลองเปลี่ยนแปลง character ใน string
-	// บรรทัดต่อไปนี้จะทำให้เกิด error เนื่องจาก string ใน Go เป็น immutable
-	// str[0] = 'h'  // ไม่สามารถทำได้
 
-	// วิธีที่ถูกต้องในการเปลี่ยนแปลง string คือการสร้าง string ใหม่
-	newStr := "h" + str[1:]
+	var a int = 20
+	var b int = 6
 
-	x := "\"hello golang\"\n I Love"
-	y := `"Hello golang"\n I Love`
-
-	fmt.Println(x)
-	fmt.Println()
-	fmt.Println(y)
-	fmt.Println(newStr) // จะแสดงผล "hello, world!"
+	fmt.Println("a + b = ", a+b)
+	fmt.Println("a - b = ", a-b)
+	fmt.Println("a * b = ", a*b)
+	fmt.Println("a / b = ", a/b)
+	fmt.Println("a % b = ", a%b)
 
 }
