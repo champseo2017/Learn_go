031780b5d2b3134503ff0c4ba7bf775790536c5b
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-01-21 23:00:10 +0700

    ประกาศตัวแปรเป็นชนิดข้อมูลบูลีน

--- GoProject/helloworld.go
+++ GoProject/helloworld.go
@@ -5,17 +5,10 @@ import (
 )
 
 func main() {
-	// ประกาศตัวแปรเป็นชนิดข้อมูลแบบ byte และ rune
-	s1 := "abcd"
-	s2 := "Hello"
+	// ประกาศตัวแปรเป็นชนิดข้อมูลบูลีน
+	var bool_val1 bool
+	bool_val2 := 1 < 2
 
-	byte_s := []byte(s1) // แปลง s1 เป็น slice ของ byte
-	rune_s := []rune(s2) // แปลง s2 เป็น slice ของ rune
-
-	// ใช้ fmt.Printf สำหรับการจัดรูปแบบข้อความ
-	// %c แสดงข้อมูลในรูปแบบของ character หรือ rune
-	// %T แสดงประเภทของตัวแปร
-	// %v แสดงค่าของตัวแปรในรูปแบบ default
-	fmt.Printf("Type of %c is %T and value is %v\n", byte_s, byte_s, byte_s)
-	fmt.Printf("Type of %c is %T and value is %v\n", rune_s, rune_s, rune_s)
+	fmt.Printf("bool_val1 is %v\n", bool_val1)
+	fmt.Printf("bool_val2 is %v\n", bool_val2)
 }
