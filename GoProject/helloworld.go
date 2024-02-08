4c176dbc4a2408255ff40d7cf4f1356369e8e65d
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-02-08 23:57:57 +0700

    ParseFloat() แปลงค่าสตริงไปเป็นเลขทศนิยม

--- GoProject/helloworld.go
+++ GoProject/helloworld.go
@@ -6,20 +6,17 @@ import (
 )
 
 /*
- ฟังค์ชั่น ParseUint() แปลงสตริงไปเป็นเลขจำนวนเต็ม แบบไม่คิดเครื่องหมาย เก็บได้เฉพาะจำนวนเต็ม บวก
+ ParseFloat() แปลงค่าสตริงไปเป็นเลขทศนิยม
 
 */
 
 func main() {
 
-	var str = "-65535"
-	var str2 = "65535"
-	number, err := strconv.ParseInt(str, 10, 64)
-	number2, err := strconv.ParseUint(str2, 10, 32)
-
+	var str = "3.14156"
+	number, err := strconv.ParseFloat(str, 64)
+	
 	if err == nil {
-		fmt.Printf("string \"%s\" with int32 base 10 : %d\n", str, number)
-		fmt.Printf("string \"%s\" with uint32 base 10 : %d\n", str, number2)
+		fmt.Printf("string \"%s\" with int32 base 10 : %f\n", str, number)
 	} else {
 		fmt.Printf("Error is %v\n", err)
 	}
