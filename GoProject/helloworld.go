46b999cfddccad98cc9e187d54b9389af2f38fda
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-02-18 21:24:31 +0700

    การใช้ Sprintf() แปลงค่าเลขจำนวนเต็มไปเป็นสตริง

--- GoProject/helloworld.go
+++ GoProject/helloworld.go
@@ -2,28 +2,33 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 /*
- แปลงค่าเลขจำนวนเต็มไปเป็นสตริง
 
- Itoa()
- FormatInt()
- FormatUint()
+ การใช้ Sprintf() แปลงค่าเลขจำนวนเต็มไปเป็นสตริง
+
+ - `fmt.Scanf` เป็นฟังก์ชันในภาษา Go ที่ใช้สำหรับอ่านข้อมูลที่ป้อนเข้ามาจาก standard input (stdin) ตามรูปแบบที่กำหนด ในกรณีนี้ "%d %d %d" หมายถึง รูปแบบการรับข้อมูลเป็นจำนวนเต็มทั้งสามตัว และจะเก็บค่าเหล่านั้นไว้ในตัวแปร num1, num2, และ num3 ตามลำดับ
+
+- `fmt.Sprintf` เป็นฟังก์ชันที่ใช้สร้าง string จากข้อมูลที่กำหนดตามรูปแบบที่ระบุ เช่นในกรณีนี้ "%d, %d and %d" ใช้สำหรับสร้าง string จากตัวเลขทั้งสามที่รับมา โดยจะเปลี่ยนค่าตัวเลขให้เป็นข้อความตามรูปแบบที่กำหนดและเก็บไว้ในตัวแปร str
+
+- `%T` เป็น verb ที่ใช้ในฟังก์ชันที่เกี่ยวข้องกับการพิมพ์ข้อมูลในภาษา Go (เช่น `fmt.Printf`) สำหรับแสดงประเภทของตัวแปร ในตัวอย่างนี้ ใช้ `%T` เพื่อแสดงประเภทของตัวแปร `str` ซึ่งจะแสดงผลว่าเป็น `string`
 
 */
 
 func main() {
+   
+	var num1, num2, num3 int
 
-	s1 := strconv.Itoa(97)
-	s2 := strconv.FormatInt(97, 10)
-	s3 := strconv.FormatInt(-97, 16)
-	s4 := strconv.FormatUint(97, 16)
+	fmt.Print("Please enter 3 numbers : ")
+	_, err := fmt.Scanf("%d %d %d", &num1, &num2, &num3)
 
-	fmt.Printf("Itoa() \"%s\"\n", s1)
-	fmt.Printf("FormatInt() \"%s\"\n", s2)
-	fmt.Printf("FormatInt() \"%s\"\n", s3)
-	fmt.Printf("FormatUint() \"%s\"\n", s4)
+	if err == nil {
+		str := fmt.Sprintf("%d, %d and %d", num1, num2, num3)
+		fmt.Printf("Type of str is %T, value is %s\n", str, str)
+	} else {
+		fmt.Printf("Error is \"%v\"\n", err)
+	}
+	
 
 }
