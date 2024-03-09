6c18c15eee46370ef5b29693079759734057d546
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-03-10 00:30:35 +0700

    Derived types เมธอด struct
    "เมธอด" ที่เชื่อมโยงกับ struct ในภาษา Go. เมธอดนี้ถูกกำหนดให้กับ struct ชื่อ Dog และใช้เพื่อกำหนดพฤติกรรมหรือการดำเนินการที่สามารถทำได้กับอินสแตนซ์ของ struct

--- GoProject/helloworld.go
+++ GoProject/helloworld.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 /*
 
 Derived types
@@ -20,8 +25,54 @@ type Person struct {
 	Age int
 }
 
+// ฟังก์ชัน (Functions)
+/* 
+// วิธีการทำงาน
+// 1. ฟังก์ชัน greet รับพารามิเตอร์ name ที่เป็น string
+// 2. ฟังก์ชันจะส่งค่ากลับเป็น string ที่เป็นการรวมคำว่า "Hello, " กับค่าของ name
+// 3. เมื่อเรียกใช้ฟังก์ชัน greet และส่งค่า name เข้าไป เช่น greet("Alice") จะได้ผลลัพธ์คือ "Hello, Alice"
+*/
+func greet(name string) string {
+	return "Hello, " + name
+}
+
+// สไลซ์ (Slices)
+// ทำไมต้องกำหนด range for _, number := range numbers
+// 1. range ใช้เพื่อวนซ้ำ (iterate) ผ่านทุกสมาชิกใน slice, array, map หรือ string
+// 2. ในการวนซ้ำผ่าน slice หรือ array, range จะส่งคืนค่าสองตัวในแต่ละรอบของ loop: index และค่าของสมาชิก
+func sum(numbers []int) int {
+	total := 0
+	for _, number := range numbers {
+		total += number
+	}
+	return total
+}
+
+// อินเทอร์เฟซ (Interfaces)
+type Animal interface {
+	Speak() string
+}
+
+// กำหนดโครงสร้างของ Dog ที่มีชื่อเป็นสตริง
+type Dog struct {
+    Name string
+}
+
+// กำหนดเมธอด Speak สำหรับ struct Dog
+func (d Dog) Speak(times int) string {
+    // ใช้ฟิลด์ Name จาก struct Dog ในการสร้างข้อความ
+    // สร้างข้อความที่ประกอบด้วยชื่อของ Dog ตามด้วย " says Woof! "
+    // และใช้ strings.Repeat เพื่อซ้ำข้อความนี้ตามจำนวนครั้งที่ระบุในพารามิเตอร์ times
+    woof := strings.Repeat(d.Name + " says Woof! ", times)
+    // ส่งคืนข้อความที่สร้างขึ้น
+    return woof
+}
+
 func main() {
-   
-	
-	
+   myDog := Dog{Name: "Buddy"}
+   // เรียกใช้เมธอด Speak โดยส่งจำนวนครั้งที่ต้องการให้ Dog "Woof!"
+   sound := myDog.Speak(3) // ต้องการให้ Dog "Woof!" 3 ครั้ง
+
+   // แสดงผลลัพธ์
+   fmt.Println(sound) // จะแสดง "Buddy says Woof! Buddy says Woof! Buddy says Woof! "
 }
