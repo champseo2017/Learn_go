556a26c702320fa046778634473d541534c8449d
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-03-18 01:14:17 +0700

    Interface และ Struct ต่างกันยังไง

--- GoProject/helloworld.go
+++ GoProject/helloworld.go
@@ -1,44 +1,26 @@
 package main
 
-import "fmt"
-
-// ประเภทอินเทอร์เฟซ (Interface types)
+// Interface ใน Go เป็นชุดของ method signatures ที่ไม่มีการเนื้อหาหรือการทำงานของ method ใดๆ กำหนดไว้
 type Greeter interface {
-    Greet() string
+    Greet() string // ตัวอย่างของ method signature ใน interface
 }
 
-// ประกาศโครงสร้าง EnglishSpeaker ที่จะเป็นหนึ่งในผู้รับใช้งานอินเทอร์เฟซ Greeter
-type EnglishSpeaker struct {}
-// ประกาศเมธอด Greet สำหรับ EnglishSpeaker ที่ตอบกลับด้วยข้อความ "Hello!"
+// Struct ใน Go เป็นชุดของ fields ที่สามารถมีทั้งข้อมูลและ methods
+// สามารถใช้ struct เพื่อ "implement" interface โดยการเขียน method ที่ตรงกับ signature ใน interface
+type EnglishSpeaker struct {} // ตัวอย่างของ struct ที่ไม่มี fields
+
+// การ implement interface ใน struct
+func (es EnglishSpeaker) Greet() string {
+    return "Hello" // ตัวอย่างของการ implement method จาก interface ใน struct
+}
+
 /* 
-func: คีย์เวิร์ดที่ใช้ในการประกาศฟังก์ชันหรือเมธอดใน Go
-(e EnglishSpeaker): ส่วนนี้เรียกว่า "receiver" ซึ่งระบุว่าเมธอดนี้เป็นของโครงสร้าง EnglishSpeaker. ตัวแปร e ในที่นี้เป็นอินสแตนซ์ของ EnglishSpeaker ที่เมธอด Greet จะทำงานกับมัน
-Greet(): ชื่อของเมธอด
-string: ประเภทของค่าที่เมธอดนี้คืนกลับ ในที่นี้คือ string
+- Interface ใช้สำหรับกำหนดชุดของ method signatures ที่ struct หรือ type อื่นๆ ต้อง implement
+- Struct ใช้สำหรับกำหนดข้อมูลและสามารถมี methods ที่ implement จาก interface หรือเป็น methods ของตัวมันเอง
+- การใช้ interface ช่วยให้สามารถเขียนโค้ดที่ flexible และ reusable มากขึ้น โดยไม่ต้องกังวลเกี่ยวกับรายละเอียดการ implement ของแต่ละ type
+
 */
-func (e EnglishSpeaker) Greet() string {
-    return "Hello!"
-}
-
-// ประกาศโครงสร้าง ThaiSpeaker ที่จะเป็นหนึ่งในผู้รับใช้งานอินเทอร์เฟซ Greeter
-type ThaiSpeaker struct{}
-
-// ประกาศเมธอด Greet สำหรับ ThaiSpeaker ที่ตอบกลับด้วยข้อความ "สวัสดี!"
-func (t ThaiSpeaker) Greet() string {
-    return "สวัสดี!"
-}
-
-// ฟังก์ชันที่รับอินเทอร์เฟซ Greeter เป็นพารามิเตอร์ และแสดงข้อความที่ได้จากเมธอด Greet
-func greetSomeone(g Greeter) {
-    fmt.Println(g.Greet())
-}
 
 func main() {
-   englishSpeaker := EnglishSpeaker{}
-   thaiSpeaker := ThaiSpeaker{}
-
-   // เรียกใช้ฟังก์ชัน greetSomeone กับ EnglishSpeaker
-   greetSomeone(englishSpeaker)
-   // เรียกใช้ฟังก์ชัน greetSomeone กับ ThaiSpeaker
-   greetSomeone(thaiSpeaker)
+   
 }
