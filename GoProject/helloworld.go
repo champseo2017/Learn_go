a49018759e65b642c7e6c999b6ac9a11b3c523d0
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-04-21 15:55:25 +0700

    Slice ใน Go เป็น reference type และอ้างอิงไปยัง array ที่อยู่เบื้องหลัง

--- GoProject/helloworld.go
+++ GoProject/helloworld.go
@@ -3,27 +3,29 @@ package main
 import "fmt"
 
 /*
-Go เราสามารถสร้าง slice ใหม่จาก slice ที่มีอยู่แล้วได้ โดยระบุ index ต่ำสุดและสูงสุดของ slice ต้นทางในรูปแบบเดียวกับการสร้าง slice จาก array
+Slice ใน Go เป็น reference type และอ้างอิงไปยัง array ที่อยู่เบื้องหลัง ดังนั้น การแก้ไของค์ประกอบของ slice จะส่งผลต่อการแก้ไของค์ประกอบที่สอดคล้องกันใน array ที่ถูกอ้างอิง และ slice อื่นๆ ที่อ้างอิงไปยัง array เดียวกันก็จะเห็นการแก้ไขนั้นด้วยเช่นกัน
 */
 
 func main() {
-	a := [8]string{"Shanghai", "Hiroshima", "Jaipur", "Gorakhpur", "Nagpur", "Mumbai", "Nashik", "Lucknow"}
-	ind_cities := a[2:]
-	maha_cities := ind_cities[2:5]
-	fmt.Println("Cities =", a)
-	fmt.Println("Indian Cities =", ind_cities)
-	fmt.Println("Maharashtra Cities =", maha_cities)
+	a := [7]string{"One", "Two", "Three", "Four", "Five", "Six", "Seven"}
+	slice1 := a[1:]
+	slice2 := a[3:]
+
+	fmt.Println("***** Before Modifications *****")
+	fmt.Println("Array =", a)
+	fmt.Println("Slice1 =", slice1)
+	fmt.Println("Slice2 =", slice2)
+
+	slice1[0] = "TWO"
+	slice1[2] = "FOUR"
+	slice2[1] = "FIVE"
+
+	fmt.Println("\n***** After Modifications *****")
+	fmt.Println("Array =", a)
+	fmt.Println("slice1 = ", slice1)
+	fmt.Println("slice2 = ", slice2)
 }
 
 /* 
-ในโปรแกรมนี้ เราสร้าง array a ที่มี 8 องค์ประกอบเป็นชื่อเมืองต่างๆ จากนั้นเราสร้าง slice ind_cities จาก array a โดยระบุ index ต่ำสุดเป็น 2 ซึ่งหมายความว่า slice ind_cities จะประกอบด้วยองค์ประกอบตั้งแต่ index 2 ถึงสุดท้ายของ array a
-
-ต่อมา เราสร้าง slice maha_cities จาก slice ind_cities โดยระบุ index ต่ำสุดเป็น 2 และ index สูงสุดเป็น 5 ซึ่งหมายความว่า slice maha_cities จะประกอบด้วยองค์ประกอบตั้งแต่ index 2 ถึง 4 ของ slice ind_cities
-
-เมื่อเราแสดงค่าของ array a, slice ind_cities และ slice maha_cities ผลลัพธ์ที่ได้คือ
-"Cities = [Shanghai Hiroshima Jaipur Gorakhpur Nagpur Mumbai Nashik Lucknow]"
-"Indian Cities = [Jaipur Gorakhpur Nagpur Mumbai Nashik Lucknow]"
-"Maharashtra Cities = [Nagpur Mumbai Nashik]"
-
-จากผลลัพธ์ จะเห็นได้ว่า slice ind_cities ประกอบด้วยองค์ประกอบตั้งแต่ index 2 ถึงสุดท้ายของ array a ซึ่งเป็นชื่อเมืองในประเทศอินเดีย และ slice maha_cities ประกอบด้วยองค์ประกอบตั้งแต่ index 2 ถึง 4 ของ slice ind_cities ซึ่งเป็นชื่อเมืองในรัฐมหาราษฏระของอินเดีย
+จากผลลัพธ์ จะเห็นได้ว่าการแก้ไขค่าใน slice1 และ slice2 ส่งผลต่อการแก้ไขค่าใน array a ด้วย เนื่องจาก slice อ้างอิงไปยัง array ที่อยู่เบื้องหลัง และเมื่อมีการแก้ไขค่าใน slice ค่าใน array ที่ถูกอ้างอิงก็จะถูกแก้ไขด้วย นอกจากนี้ slice อื่นๆ ที่อ้างอิงไปยัง array เดียวกันก็จะเห็นการแก้ไขนั้นเช่นกัน
 */
