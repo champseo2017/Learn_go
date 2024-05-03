6fcc32def1e81ae54f57996177cc3d99c1f42331
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-05-03 23:36:26 +0700

    struct เป็น value type

--- GoProject/main/main.go
+++ GoProject/main/main.go
@@ -5,19 +5,18 @@ import "fmt"
 /*
 
  */
-// อธิบายแนวคิดของพอยน์เตอร์ใน struct
-
+// struct เป็น value type
 type Person struct {
 	Name string
 	Age int
 }
  
 func main() {
-	p := Person{Name: "John", Age: 30}
-	ptr := &p
-	fmt.Println(ptr.Name)
-	ptr.Age = 31
-	fmt.Println(p.Age)
+	p1 := Person{Name: "John", Age: 30}
+	p2 := p1
+	p2.Name = "Alice"
+	fmt.Println(p1.Name)
+	fmt.Println(p2.Name)
 }
 /* 
 
