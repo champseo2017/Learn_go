3ace41ab594770f709a12511689da2903518a818
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-05-03 23:31:16 +0700

    อธิบายค่า zero value ของ struct

--- GoProject/main/main.go
+++ GoProject/main/main.go
@@ -5,15 +5,15 @@ import "fmt"
 /*
 
  */
-// ประกาศและกำหนดค่าเริ่มต้นให้กับ struct
- type Person struct {
+// อธิบายค่า zero value ของ struct
+type Person struct {
 	Name string
 	Age int
- }
-
+}
+ 
 func main() {
-	p := Person{Name: "John", Age: 30}
-	fmt.Println(p)
+	var p Person
+	fmt.Println("Name: %q, Age: %d\n", p.Name, p.Age)
 }
 /* 
 
