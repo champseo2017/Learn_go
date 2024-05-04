a1ee9120474a3103a38ea4221998e9c39c3245a6
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-05-04 23:12:04 +0700

    Method ของ Function

--- GoProject/main/main.go
+++ GoProject/main/main.go
@@ -8,18 +8,22 @@ import (
 ตัวอย่างการกำหนด Method ให้กับ Type ต่างๆ
 */
 
-// Method ของ Map
-type StringMap map[string]string
+// Method ของ Function
+type MathFunc func(int, int) int
 
-func (m StringMap) GetValue(key string) string {
-	return m[key]
+func (f MathFunc) Compute(a, b int) int {
+	return f(a, b)
+}
+
+func add(a, b int) int {
+	return a + b
 }
 
 func main() {
-	dict := StringMap{"apple": "fruit", "carrot": "vegetable"}
-	value := dict.GetValue("apple")
-	fmt.Println(value) // Output: fruit
+	mathFunc := MathFunc(add)
+	result := mathFunc.Compute(3, 5)
+	fmt.Println(result) // Output: 8
 }
 /* 
-
+จะเห็นได้ว่าเราสามารถกำหนด Method ให้กับ Type ต่างๆ ได้หลากหลาย ทั้ง Basic Types อย่าง int และ string, Composite Types เช่น Array, Slice และ Map รวมถึง Function เพื่อเพิ่มความสามารถและพฤติกรรมเฉพาะให้กับ Type เหล่านั้น การเรียกใช้ Method สามารถทำได้โดยใช้ตัวแปรหรือค่าของ Type ที่กำหนดและตามด้วยชื่อ Method และพารามิเตอร์ (ถ้ามี) ผลลัพธ์ที่ได้จะขึ้นอยู่กับการดำเนินการภายใน Method ที่กำหนดไว้
 */
