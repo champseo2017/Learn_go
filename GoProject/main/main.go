de3707ccace3afa7d5778223ba154b22d531176f
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-05-04 22:52:09 +0700

    Method ของ Basic Type (string):

--- GoProject/main/main.go
+++ GoProject/main/main.go
@@ -6,17 +6,24 @@ import "fmt"
 ตัวอย่างการกำหนด Method ให้กับ Type ต่างๆ
 */
 
-// Method ของ Basic Type (int)
-type MyInt int
+// Method ของ Basic Type (string):
+type MyString string
 
-func (m MyInt) Double() int {
-	return int(m * 2)
+func (s MyString) Reverse() string {
+    runes := []rune(s)
+	/* 
+	runes := []rune(s) เป็นการแปลง MyString เป็น Slice ของ rune ([]rune) เพื่อให้สามารถทำงานกับตัวอักษรแต่ละตัวในสตริงได้อย่างถูกต้อง โดยเฉพาะอย่างยิ่งเมื่อสตริงมีตัวอักษรที่ไม่ใช่ ASCII เช่น ตัวอักษรภาษาไทย
+	*/
+    for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+        runes[i], runes[j] = runes[j], runes[i]
+    }
+    return string(runes)
 }
 
 func main() {
-	num := MyInt(5)
-	result := num.Double()
-	fmt.Println(result)
+	str := MyString("สวัสดี")
+	reversed := str.Reverse()
+	fmt.Println(reversed) //  ีดสัวส
 }
 /* 
 
