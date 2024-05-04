b4c32c6b2bc4cabde5f2f79d7db067b824bb44e1
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-05-04 19:33:19 +0700

    Methods ในภาษา Go

--- GoProject/main/main.go
+++ GoProject/main/main.go
@@ -1,21 +1,35 @@
 package main
 
-import "fmt"
-
 /*
+1. Methods คือฟังก์ชันที่เชื่อมโยงกับ Type (ชนิดข้อมูล) เฉพาะ
+2. Methods ประกาศด้วยการเพิ่ม Receiver ระหว่างคีย์เวิร์ด `func` และชื่อ Method
+3. Receiver ระบุ Type ที่ Method เชื่อมโยงด้วย และสามารถเข้าถึงฟิลด์ของ Type นั้นได้
+4. Methods สามารถมีพารามิเตอร์และคืนค่าได้เหมือนฟังก์ชันทั่วไป
+5. ชื่อ Receiver ควรสั้นและใช้ชื่อเดียวกันสำหรับทุก Method เพื่อความสม่ำเสมอ
+6. Methods และ Type ของมันต้องถูกประกาศในแพ็คเกจเดียวกัน ไม่สามารถประกาศ Method ในแพ็คเกจที่แตกต่างจาก Type ได้
 
- */
-// ตรวจสอบความเท่ากันของ struct
+Methods ช่วยให้เราสามารถเขียนโค้ดแบบเชิงวัตถุ (Object-Oriented) ในภาษา Go ได้ โดยกำหนดพฤติกรรมเฉพาะให้กับ Type ต่างๆ และทำให้โค้ดมีความเป็นโมดูลาร์ ง่ายต่อการอ่านและบำรุงรักษา
 
-type Person struct {
-	Name string
-	Age int
+func (receiverName receiverType) methodName(param1 paramType) (returnType1) {
+    // ...
 }
- 
+*/
+
+type rectangle struct {
+	length float32
+	width float32
+}
+
+func (r rectangle) area() float32 {
+	return r.length * r.width
+}
+
+func (rect rectangle) perimeter() float32 {
+	return 2 * (rect.length + rect.width)
+}
+
 func main() {
-	p1 := Person{Name: "John", Age: 30}
-	p2 := Person{Name: "John", Age: 30}
-	fmt.Println(p1 == p2)
+	
 }
 /* 
 
