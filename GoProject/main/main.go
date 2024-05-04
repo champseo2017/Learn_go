70697ffca4e2f5d557edac0bfc0cfca21975c245
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-05-04 22:54:35 +0700

    Method ของ Array

--- GoProject/main/main.go
+++ GoProject/main/main.go
@@ -6,24 +6,21 @@ import "fmt"
 ตัวอย่างการกำหนด Method ให้กับ Type ต่างๆ
 */
 
-// Method ของ Basic Type (string):
-type MyString string
+// Method ของ Array
+type IntArray [5]int
 
-func (s MyString) Reverse() string {
-    runes := []rune(s)
-	/* 
-	runes := []rune(s) เป็นการแปลง MyString เป็น Slice ของ rune ([]rune) เพื่อให้สามารถทำงานกับตัวอักษรแต่ละตัวในสตริงได้อย่างถูกต้อง โดยเฉพาะอย่างยิ่งเมื่อสตริงมีตัวอักษรที่ไม่ใช่ ASCII เช่น ตัวอักษรภาษาไทย
-	*/
-    for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-        runes[i], runes[j] = runes[j], runes[i]
-    }
-    return string(runes)
+func (a IntArray) Sum() int {
+	sum := 0
+	for _, value := range a {
+		sum += value
+	}
+	return sum
 }
 
 func main() {
-	str := MyString("สวัสดี")
-	reversed := str.Reverse()
-	fmt.Println(reversed) //  ีดสัวส
+	arr := IntArray{1, 2, 3, 4, 5}
+	sum := arr.Sum()
+	fmt.Println(sum) // Output: 15
 }
 /* 
 
