2cfb2bf930935eb085d070deeff49bb342fd9f8c
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-05-04 20:53:54 +0700

    Value Receiver และ Pointer Receiver ในภาษา Go ต่อ

--- GoProject/main/main.go
+++ GoProject/main/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 /*
 Value Receiver และ Pointer Receiver ในภาษา Go:
@@ -21,33 +23,15 @@ type rectangle struct {
 	width float32
 }
 
-func (r rectangle) area() float32 {
-	return r.length * r.width
+func (r rectangle) increaseLength(a float32) {
+	r.length = r.length + a
 }
-
-func (rect rectangle) perimeter() float32 {
-	return 2 * (rect.length + rect.width)
-}
-
-func (rectangle) doSomething() {
-	fmt.Println("I don't know what to do")
-}
-
-/* 
-ประกาศ Method doSomething() ที่มีเฉพาะ Receiver Type เป็น rectangle โดยไม่มีชื่อ Receiver
-Method นี้ไม่ได้อ่านหรือแก้ไขฟิลด์ใดๆ ของ rectangle struct แต่เพียงแค่แสดงข้อความทางหน้าจอ
-*/
 
 func main() {
 	rect := rectangle{12.3, 21.45}
-	a := rect.area()
-	fmt.Println(a)
-	/* 
-	สร้างตัวแปร rect เป็น rectangle struct ด้วยค่า length เป็น 12.3 และ width เป็น 21.45
-	เรียกใช้ Method area() ด้วย rect.area() และเก็บผลลัพธ์ไว้ในตัวแปร a
-	แสดงค่าของ a ทางหน้าจอ
-	*/
+	rect.increaseLength(3.4)
+	fmt.Println(rect)
 }
 /* 
-สรุปได้ว่า การประกาศ Method ใน Go สามารถทำได้โดยระบุ Receiver Type และชื่อ Receiver (ถ้าจำเป็น) การเรียกใช้ Method ทำได้โดยใช้เครื่องหมายจุด (.) กับค่าของ Type ที่ Method นั้นเชื่อมโยงอยู่ และ Method สามารถอ่านหรือแก้ไขฟิลด์ของ Receiver Type ได้ โดย Method และ Type ของมันต้องอยู่ในแพ็คเกจเดียวกัน
+
 */
