79cf0b7630115591da7892da44da9bf1dfeba705
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-05-06 20:51:37 +0700

    จงตอบว่าจริงหรือเท็จ: เราสามารถสร้างเมธอดที่โอเวอร์โหลด (overloaded) ได้ใน Go คำตอบคือ

--- GoProject/main/main.go
+++ GoProject/main/main.go
@@ -1,27 +1,17 @@
 package main
 
-import "fmt"
-
 /*
+จงตอบว่าจริงหรือเท็จ: เราสามารถสร้างเมธอดที่โอเวอร์โหลด (overloaded) ได้ใน Go
+คำตอบคือ
+*/
 
- */
+// เท็จ Go ไม่รองรับการโอเวอร์โหลดเมธอด
 
-// ผลลัพธ์ของโค้ดต่อไปนี้คืออะไร
-type person struct {
-    id int
-    name string
-}
-
-func (p person) setName(name string) {
-    p.name = name
-}
 
 
 func main() {
-	p := person{101, "Xyz"}
-    p.setName("Abc")
-    fmt.Println(p)
+	
 }
 /* 
-ผลลัพธ์จะเป็น {101 Xyz} เพราะเมธอด setName ใช้ตัวรับแบบค่า (value receiver) ดังนั้นการเปลี่ยนแปลงใดๆ ที่ทำภายในเมธอดจะไม่ส่งผลต่อค่าของ p ในฟังก์ชัน main
+
 */
