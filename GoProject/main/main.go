c8087727ff90b39d540d5f5a3c71265aa7b7ab56
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-05-08 07:35:15 +0700

    ถ้า type T implement interface I โดยกำหนด pointer receiver method แสดงว่าเฉพาะ *T เท่านั้นที่จะ implement interface

--- GoProject/main/main.go
+++ GoProject/main/main.go
@@ -3,35 +3,24 @@ package main
 import "fmt"
 
 /*
-เมื่อเรา implement interface ด้วย value receiver เราสามารถกำหนดทั้ง value และ address ของ type ให้กับ interface ได้
+ถ้า type T implement interface I โดยกำหนด pointer receiver method แสดงว่าเฉพาะ *T เท่านั้นที่จะ implement interface I ไม่ใช่ T ดังนั้นเราสามารถกำหนดเฉพาะ address ของ T ให้กับตัวแปร interface เท่านั้น
+แต่ถ้า type T implement interface I โดยกำหนด value receiver method แสดงว่าทั้ง T และ *T จะ implement interface I ดังนั้นเราสามารถกำหนดได้ทั้ง value และ address ของ T ให้กับตัวแปร interface
 */
 
-type Itr interface {
-    m1()
-    m2()
+// กรณี pointer receiver
+type IPrint interface {
+    Print()
 }
 
-type St struct {
-
+type Student struct {
+    name string
 }
 
-func (s St) m1() {
-    fmt.Println("In m1 of St")
-}
-
-func (s St) m2() {
-    fmt.Println("In m2 of St")
+func (s *Student) Print() {
+    fmt.Println("Name:", s.name)
 }
 
 func main() {
-   fmt.Println("Assigning value of St")
-   i := Itr(St{})
-   i.m1()
-   i.m2()
-
-   fmt.Println("Assigning Address of St")
-   i = Itr(&St{})
-   i.m1()
-   i.m2()
-
+  i := IPrint(&Student{"John"})
+  fmt.Printf("%s", i)
 }
