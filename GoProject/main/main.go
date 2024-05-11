9df9e035b9ad8c173203f9c268cf5bdc9838eba4
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-05-11 15:44:38 +0700

    ในภาษา Go การส่ง slice เข้าไปใน function แทนการส่ง address ของ array

--- GoProject/main/main.go
+++ GoProject/main/main.go
@@ -5,16 +5,17 @@ import (
 )
 
 /*
-code ที่แสดงการสร้าง slice จาก array และส่ง slice เข้าไปใน function แทนการส่ง address ของ array โดยตรง
+ในภาษา Go การส่ง slice เข้าไปใน function แทนการส่ง address ของ array โดยตรงถือเป็นแนวปฏิบัติที่ดีกว่า เพราะ slice มีประสิทธิภาพและความยืดหยุ่นมากกว่า array และการแก้ไขค่าใน slice ก็ยังส่งผลต่อ underlying array เดิมอยู่
+
+การเปลี่ยนแปลงค่าใน slice ภายใน modify function ส่งผลต่อ arr ใน main function ด้วย เนื่องจาก slice ที่ส่งเข้าไปใน modify มี underlying data structure เป็น arr เดิม
 */
 
-func modify(slice []int) { // function ที่รับ slice ของ int
-	slice[0] = 100 // เปลี่ยนค่า element ที่ 0 ของ slice เป็น 100
+func modify(arr []int) { // function ที่รับ slice ของ int เป็น parameter
+	arr[0] = 100 // เปลี่ยนค่า element ที่ 0 ของ slice เป็น 100
 }
 
 func main() {
-	arr := [5]int{10, 20, 30, 40, 50}   // ประกาศและกำหนดค่าเริ่มต้นให้กับ array ของ int ขนาด 5 ตัว
-	slice := arr[:]                     // สร้าง slice จาก arr โดยใช้ index เริ่มต้นและสิ้นสุดเป็นค่าเริ่มต้น (0 ถึง length ของ array - 1)
-	modify(slice)                       // ส่ง slice เข้าไปใน modify function
-	fmt.Println("Array Elements:", arr) // แสดงค่า element ของ arr
+	arr := [5]int{10, 20, 30, 40, 50}  // ประกาศและกำหนดค่าเริ่มต้นให้กับ array arr
+	modify(arr[:])                     // สร้าง slice จาก arr ด้วย arr[:] และส่งเข้าไปใน modify function
+	fmt.Println("Slice Elements", arr) // แสดงค่า element ของ arr
 }
