4e0c9fb8064337a07cc4924ea0954e957dc710df
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-05-25 13:12:10 +0700

    ch ซึ่งเป็น channel แบบเขียนอย่างเดียว, ch ซึ่งเป็น channel แบบอ่านอย่างเดียว

--- GoProject/main/main.go
+++ GoProject/main/main.go
@@ -1,41 +1,20 @@
 package main
 
-import (
-	"fmt"
-)
-
 /*
-ตัวอย่างของการใช้ unbuffered channel ในการสื่อสารระหว่าง goroutine ของ producer และ consumer โดย producer จะทำการส่งข้อมูลเข้าไปใน channel และ consumer จะทำการอ่านข้อมูลจาก channel ซึ่งการอ่านและเขียนข้อมูลผ่าน unbuffered channel จะเกิดขึ้นแบบ synchronous กล่าวคือ เมื่อ producer ส่งข้อมูล จะต้องรอให้ consumer อ่านข้อมูลก่อน จึงจะสามารถส่งข้อมูลต่อไปได้ และในทำนองเดียวกัน consumer จะต้องรอให้ producer ส่งข้อมูลเข้ามาก่อน จึงจะสามารถอ่านข้อมูลได้
-*/
 
-func producer(ch chan int, done chan bool) {
-	for index := 0; index < 10; index++ {
-		fmt.Printf("PRODUCER: sending %v\n", index)
-		ch <- index // ส่งค่า index เข้าไปใน channel
-	}
-	close(ch)    // ปิด channel เมื่อส่งข้อมูลครบแล้ว
-	done <- true // ส่งสัญญาณว่า producer ทำงานเสร็จแล้ว
+ */
+
+func readVal(ch <-chan int) {
+	// ฟังก์ชัน readVal รับพารามิเตอร์ ch ซึ่งเป็น channel แบบอ่านอย่างเดียว
+	ch <- 10 // พยายามส่งค่า 10 เข้าไปใน channel ch ซึ่งจะทำให้เกิด compilation error
 }
 
-func consumer(ch chan int) {
-	for val := range ch { // อ่านข้อมูลจาก channel จนกว่าจะปิด
-		fmt.Printf("CONSUMER: read %v\n", val)
-	}
+func writeVal(ch chan<- int) {
+	// ฟังก์ชัน writeVal รับพารามิเตอร์ ch ซึ่งเป็น channel แบบเขียนอย่างเดียว
+	<-ch // พยายามอ่านค่าจาก channel ch ซึ่งจะทำให้เกิด compilation error
 }
-
 func main() {
-	ch := make(chan int)    // สร้าง unbuffered channel สำหรับส่งข้อมูลชนิด int
-	done := make(chan bool) // สร้าง channel สำหรับส่งสัญญาณเมื่อ producer ทำงานเสร็จ
 
-	go producer(ch, done) // สร้าง goroutine สำหรับ producer
-	go consumer(ch)       // สร้าง goroutine สำหรับ consumer
-
-	<-done // รอจนกว่า producer จะทำงานเสร็จ
-	/*
-		<-done ในฟังก์ชัน main() มีความสำคัญในการรอให้ goroutine ของ producer ทำงานเสร็จก่อนที่โปรแกรมจะจบการทำงาน
-		เนื่องจากในภาษา Go เมื่อเรียกใช้ goroutine โปรแกรมจะไม่รอให้ goroutine นั้นทำงานเสร็จ แต่จะทำงานต่อไปเรื่อยๆ จนกว่าจะจบฟังก์ชัน main() ดังนั้น หากไม่มีบรรทัด <-done โปรแกรมอาจจบการทำงานก่อนที่ producer จะทำงานเสร็จ ทำให้ไม่ได้ผลลัพธ์ตามที่ต้องการ
-		บรรทัด <-done เป็นการรอรับสัญญาณจาก done channel ซึ่ง producer จะส่งสัญญาณมาเมื่อทำงานเสร็จแล้ว การรอรับสัญญาณนี้จะทำให้ goroutine ของ main() หยุดรอจนกว่าจะได้รับสัญญาณ ซึ่งก็คือรอจนกว่า producer จะทำงานเสร็จนั่นเอง
-	*/
 }
 
 /*
