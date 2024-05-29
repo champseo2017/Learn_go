c8b377caa4f0849246d8ed93cc1d7abed935fc29
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-05-29 08:12:42 +0700

    เรียกใช้ฟังก์ชัน recover โดยไม่ได้ใช้ร่วมกับ defer

--- GoProject/main/main.go
+++ GoProject/main/main.go
@@ -5,35 +5,31 @@ import (
 )
 
 /*
-คำสั่ง defer ใน Go จะสร้างฟังก์ชันขึ้นมาแต่จะไม่เรียกใช้จนกว่าฟังก์ชันที่สร้าง defer จะจบลง โดยจะเรียกใช้ตามลำดับกลับกันจากการสร้าง
+เรียกใช้ฟังก์ชัน recover โดยไม่ได้ใช้ร่วมกับ defer มันจะไม่มีผลอะไรเลย และจะ return ค่า nil เสมอ
 */
 
-// ฟังก์ชัน fn1()
 func fn1() {
-	fmt.Println("Start of Fn1 Func") // พิมพ์ "Start of Fn1 Func" ก่อน
-
-	// defer ฟังก์ชันถูกสร้างขึ้นและจะถูกเรียกเมื่อ fn1() จบลง
+	fmt.Println("Start of Fn1 Func")
+	fmt.Println("Recovered", recover()) // recover จะ return nil เพราะไม่ได้ใช้ร่วมกับ defer
 	defer func() {
-		fmt.Println("Defer in Fn1 func")     // พิมพ์ "Defer in Fn1 func" ก่อน
-		fmt.Println("Recovered:", recover()) // พิมพ์ "Recovered: <nil>" เนื่องจากไม่มี panic เกิดขึ้น
+		fmt.Println("Defer in Fn1 func")
 	}()
-
-	fmt.Println("End of Fn1 Func") // พิมพ์ "End of Fn1 Func" ออกมาก่อนจบ fn1()
+	panic("Panicing from fn1") // เรียก panic ใน fn1
 }
 
 func main() {
-	fmt.Println("Start of Main Func") // พิมพ์ "Start of Main Func" ออกมาก่อน
-
-	// defer ฟังก์ชันถูกสร้างขึ้นและจะถูกเรียกเมื่อ main() จบลง
+	fmt.Println("Start of Main Func")
 	defer func() {
 		fmt.Println("Defer in Main func")
 	}()
-
-	fn1() // เรียกใช้ fn1()
-
-	fmt.Println("End of Main Func") // พิมพ์ "End of Main Func" ออกมาก่อนจบ main()
+	fn1()
+	fmt.Println("End of Main Func")
 }
 
 /*
-
- */
+- ฟังก์ชัน `recover` จะไม่มีผลใดๆ หากเรียกใช้โดยไม่ได้ใช้ร่วมกับ `defer` และจะ return ค่า `nil` เสมอ
+- ใน `main` มีการเรียกใช้ `defer` เพื่อแสดงข้อความหลังจาก `main` จบการทำงาน
+- เรียกใช้ `fn1` ซึ่งมีการเรียก `recover` แต่ไม่มีผล เพราะไม่ได้ใช้ร่วมกับ `defer`
+- ใน `fn1` มีการเรียก `defer` เพื่อแสดงข้อความก่อนจบฟังก์ชัน และเรียก `panic` ทำให้โปรแกรมหยุดทำงานทันที
+- `defer` ใน `main` จะทำงานก่อนโปรแกรมจบการทำงาน
+*/
