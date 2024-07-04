83fb61a6e296c6311afc984dc5ecb6671891d7fd
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-07-04 21:35:04 +0700

    defer มักใช้เพื่อปิดไฟล์หรือจัดการ I/O ต่างๆ ส่วน panic() ใช้เพื่อหยุดโปรแกรมเมื่อเจอข้อผิดพลาดร้ายแรง

--- GoProject/main/main.go
+++ GoProject/main/main.go
@@ -6,50 +6,21 @@ import (
 )
 
 /*
-สรุปตัวอย่างการใช้งานฟังก์ชัน `deferExample()` และ `demoErrorHandling()` สั้นๆ ดังนี้
-
-1. ฟังก์ชัน `deferExample()`:
-   - แสดงลำดับการทำงานของคำสั่ง `defer`
-   - คำสั่งที่อยู่หลัง `defer` จะถูกเลื่อนไปทำหลังสุด
-
-2. ฟังก์ชัน `demoErrorHandling()`:
-   - แสดงการจัดการ Error ด้วย `defer` และ `recover()`
-   - เมื่อเกิด Panic (เช่น หารด้วย 0) `defer` จะทำงานทันทีและแสดงข้อความ Error
-
-3. ฟังก์ชัน `main()`:
-   - เรียกใช้ฟังก์ชัน `deferExample()` และ `demoErrorHandling()` เพื่อแสดงตัวอย่างการทำงาน
-
-สรุปคือ `defer` ใช้เพื่อเลื่อนการทำงานของคำสั่งและใช้จัดการ Error ร่วมกับ `recover()` ส่วน `recover()` ใช้ดักจับ Panic ที่เกิดขึ้นระหว่างการทำงานของโปรแกรม
-
-defer จะถูกจัดเก็บเป็น Stack และทำงานแบบ Last In First Out (LIFO) นั่นคือ defer ที่ถูกเรียกทีหลังสุดจะทำงานก่อน
+1. ในโค้ดมีการใช้คำสั่ง defer เพื่อให้โค้ดทำงานเป็นอันดับสุดท้าย และใช้ panic() เพื่อหยุดโปรแกรมเมื่อเกิดข้อผิดพลาด
+2. defer มักใช้เพื่อปิดไฟล์หรือจัดการ I/O ต่างๆ ส่วน panic() ใช้เพื่อหยุดโปรแกรมเมื่อเจอข้อผิดพลาดร้ายแรง
+3. Go มีคำสั่ง defer และ panic() เพื่อช่วยให้โปรแกรมทำงานได้อย่างสมบูรณ์และจัดการกับข้อผิดพลาดได้
 */
 
-func deferExample() {
-	fmt.Println("This is example 1")
-	defer fmt.Println("This is example 2") // ถูกเรียกเป็นลำดับที่ 2
-	fmt.Println("This is example 3")
-}
-
-func demoErrorHandling() {
-	fmt.Println("----Demo Error Handling----")
-	defer func() { // ถูกเรียกเป็นลำดับแรก
-		if err := recover(); err != nil {
-			fmt.Println("Error:", err)
+func main() {
+	defer func() {
+		fmt.Println("Code before panic")
+		if r := recover(); r != nil {
+			fmt.Println("Panic attack!!")
 		}
 	}()
 
-	// เกิด Panic เนื่องจากหารด้วย 0
-	valZero := 0
-	result := 10 / valZero
-	fmt.Println("Result:", result)
-}
-
-func main() {
-	fmt.Println("==== defer Example ====")
-	deferExample()
-
-	fmt.Println("\n==== Error Handling Example ====")
-	demoErrorHandling()
+	fmt.Println("Code after panic")
+	panic("panic has been raised")
 }
 
 /*
